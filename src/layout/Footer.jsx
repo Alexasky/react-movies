69a6a6d8476ee0f5467e08c9ca8c814bd570e578
Alex Korenkova <alex.korenkova@gmail.com>
2023-05-01 00:43:44 +0300

    repo link added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,11 +1,20 @@
 function Footer() {
-	return <footer className="page-footer green lighten-1">
-		<div className="footer-copyright">
-			<div className="container">
-				©{new Date().getFullYear()} Copyright Text
-				<a className="grey-text text-lighten-4 right" href="#!">Repo</a>
-			</div>
-		</div>
-	</footer>
+    return (
+        <footer className='page-footer green lighten-1'>
+            <div className='footer-copyright'>
+                <div className='container'>
+                    ©{new Date().getFullYear()} Copyright Text
+                    <a
+                        className='grey-text text-lighten-4 right'
+                        href='https://github.com/Alexasky/react-movies'
+                        rel='noreferrer'
+                        target='_blank'
+                    >
+                        Repo
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
 }
-export { Footer }
+export { Footer };
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,26 @@
 function Header() {
-	return <nav className='green darken-1'>
-		<div className="nav-wrapper">
-			<a href="#" className="brand-logo">React Movies</a>
-			<ul id="nav-mobile" className="right hide-on-med-and-down">
-				<li><a href="#!">Repo</a></li>
-
-			</ul>
-		</div>
-	</nav>
+    return (
+        <nav className='green darken-1'>
+            <div className='nav-wrapper'>
+                <a
+                    href='https://github.com/Alexasky/react-movies'
+                    className='brand-logo'
+                >
+                    React Movies
+                </a>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <a
+                            href='https://github.com/Alexasky/react-movies'
+                            rel='noreferrer'
+                            target='_blank'
+                        >
+                            Repo
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
-export { Header }
+export { Header };
